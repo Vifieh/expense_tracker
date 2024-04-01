8cfa7cf77b3e816036d773f3dbb36f2f94ebdd6c
Vifieh <vifiehruth@gmail.com>
2024-04-01 14:33:25 +0100

    expense tracker completion

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,10 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  // ]).then((fn) {
   runApp(
     MaterialApp(
       darkTheme: ThemeData.dark().copyWith(
@@ -57,4 +61,5 @@ void main() {
       home: const Expenses(),
     ),
   );
+  // });
 }
